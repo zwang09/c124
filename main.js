ed73c95245df6324eac7c1959068d762e8811fe1
zwang09 <91443333+zwang09@users.noreply.github.com>
2022-12-10 18:37:24 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized!')
+}
+
+function draw() {
+    background('#969A97');
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results)
+    }
+}
